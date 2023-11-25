8eec95438cd28ce1c54b2c2b8c28b869bb99071f
woodie <woodie@woodieui-MacBookAir.local>
2023-11-25 11:55:46 +0900

    Add dissolve animation.

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const dissolve = keyframes`
+  0%{opacity: 0}
+  50%{opacity: 1}
+  100%{opacity: 0}
+`;
+const AnimatedParagraph = styled.p`
+  animation: ${dissolve} 2s infinite;
+  color: #ffffff;
+`;
 
 function Intro() {
   return (
-    <div className="Intro">
-      <p>
-        This is <b>Intro</b> page
-      </p>
+    <div
+      className="Main"
+      style={{
+        position: "absolute",
+        backgroundColor: "black",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <AnimatedParagraph> Logo </AnimatedParagraph>
     </div>
   );
 }
