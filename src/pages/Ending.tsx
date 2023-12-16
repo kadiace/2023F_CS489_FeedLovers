a544a2548e1b86e12d53b279d25dc92eef600c41
Haeseul <jjchsjj@gmail.com>
2023-12-17 04:57:53 +0900

    Add ending animation

--- src/pages/Ending.tsx
+++ src/pages/Ending.tsx
@@ -1,4 +1,6 @@
-import { EndingAnimation } from "components/Animation";
+import EndAnimation from "assets/img/end_animation.gif";
+
+import { EndingAnimation, EndingGifAnimation } from "components/Animation";
 import ConsumerGroup from "components/ConsumerGroup";
 import GuideWindow from "components/GuideWindow";
 import { MouseEventHandler, useState } from "react";
@@ -8,11 +10,17 @@ function Ending() {
   // state
   /* esline-disable */
   const [afterGIF, setAfterGIF] = useState(false);
+  const [beforeGIF, setBeforeGIF] = useState(false);
+  const [maskConsumer, setMaskConsumer] = useState(true);
 
   // const
   const navigate = useNavigate();
   const navigateLobby: MouseEventHandler = () => {
     navigate("/lobby");
+  };
+  const turnOnGif = () => {
+    setMaskConsumer(false);
+    setBeforeGIF(true);
   };
   const turnOnPopUp = () => {
     setAfterGIF(true);
@@ -36,23 +44,47 @@ function Ending() {
               backgroundColor: "black",
               width: "100%",
               height: "100%",
-              opacity: 0.6,
-              zIndex: 9,
-            }}
-          />
-          <GuideWindow
-            messageList={["> TEST: Ending Script."]}
-            navigate={navigateLobby}
-            style={{
-              display: "flex",
-              position: "absolute",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "100%",
-              height: "100%",
+              opacity: 1,
               zIndex: 10,
             }}
-          />
+          >
+            <GuideWindow
+              messageList={["> TEST: Ending Script."]}
+              navigate={navigateLobby}
+              style={{
+                display: "flex",
+                position: "absolute",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                height: "100%",
+                zIndex: 11,
+              }}
+            />
+          </div>
+        </>
+      )}
+      {beforeGIF && (
+        <>
+          <div
+            style={{
+              position: "relative",
+              left: "100px",
+              top: "-25px",
+              width: "100%",
+              height: "100%",
+              objectFit: "contain",
+              justifyContent: "center",
+              zIndex: 9,
+            }}
+          >
+            <EndingGifAnimation
+              onAnimationEnd={turnOnPopUp}
+              path={EndAnimation}
+              delay="0s"
+              duration="13.2s"
+            />
+          </div>
         </>
       )}
       <div
@@ -64,12 +96,16 @@ function Ending() {
           justifyContent: "center",
         }}
       >
-        <ConsumerGroup />
-        <EndingAnimation
-          onAnimationEnd={turnOnPopUp}
-          delay="1s"
-          duration="5s"
-        />
+        {maskConsumer && (
+          <>
+            <ConsumerGroup />
+            <EndingAnimation
+              onAnimationEnd={turnOnGif}
+              delay="1s"
+              duration="5s"
+            />
+          </>
+        )}
       </div>
     </div>
   );
